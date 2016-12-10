5c06049f83524c8cc5874e2b424c8ce83c04d571
keith brooks <kbrook10@gmail.com>
2016-12-10 17:42:20 -0500

    Ran into issue with columns not originally being declared in the model...Need to revert back to older version of git

--- false_prophet/app/Http/Controllers/PostController.js
+++ false_prophet/app/Http/Controllers/PostController.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const Post = use('App/Model/Post')
+
 class PostController {
-    * index (request, response) {
-      yield response.sendView('home')
-}
+    * index(request, response) {
+        const posts = yield Post.all()
+        yield response.sendView('home', {posts: posts.toJSON()})
+    }
 }
 
 module.exports = PostController
