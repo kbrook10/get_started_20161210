ea993da42e2ee9c74a356cc147ba9a3a646744fb
keith brooks <kbrook10@gmail.com>
2016-12-10 16:27:23 -0500

    Update to create database to receive posts

---
+++ false_prophet/database/migrations/1481403367622_posts.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostsTableSchema extends Schema {
+
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+
+}
+
+module.exports = PostsTableSchema
